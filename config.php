27936093f1af6d69ed5a11c41668ffdc957d11e4
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-28 21:17:30 +0000

    Initial creation
    Add DB configuration code

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'nkwf3.h.filess.io');
+define('DB_USER', 'COSC4806_undersort');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', 'COSC4806_undersort');
+define('DB_PORT', '61000');
+
+?>
