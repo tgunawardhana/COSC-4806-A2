c32b2b9c9540efe137bd50fb25df423a9e4e18ff
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-28 22:40:48 +0000

    Add code  to define class User
    Add code to connect to the database and run query

--- user.php
+++ user.php
@@ -1,6 +1,16 @@
 <?php
 
+Class User {
 
+  public function get_all_users() {
+    $dbh = db_connect();
+    $statement = $dbh->prepare("select * from users;");
+    $statement->execute();
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
+    return $rows;
+  }
+  
+}
 
 
 ?>
