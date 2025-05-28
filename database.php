19a219cdc6f23f6a9b86c650c24765a048538601
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-28 22:46:19 +0000

    Add missing braces to the code

--- database.php
+++ database.php
@@ -8,7 +8,9 @@ function db_connect(){
      $dbh = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT .';dbname='.DB_DATABASE, DB_USER, DB_PASS);
      return $dbh;
    }
-   catch (PDOException $e)
+   catch (PDOException $e) {
+     
+   }
 
 }
 
