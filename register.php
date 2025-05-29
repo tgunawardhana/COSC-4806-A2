4f69622ee3e32b66f3385c007420fecc09379b6d
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-29 02:31:47 +0000

    Add code with a form to sign up

---
+++ register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign Up</title>
+  </head>
+  <body>
+
+    <h1>Sign Up</h1>
+
+    <form action="/user_registration.php" method="post">
+      <label for="fname">Username:</label>
+      <br>
+      <input type="text" id="username" name="username">
+      <br>
+      <label for="password">Password:</label>
+      <br>
+      <input type="password" id="password" name="password">
+      <br><br>
+      <input type="submit" value="Sign Up">
+      <br>
+    </form>
+
+  </body>
+</html>
+
+
+
