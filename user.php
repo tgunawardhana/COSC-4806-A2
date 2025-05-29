bdcf74094a1d931f68cb53bb3cb38f027eb73c0c
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-29 01:57:33 +0000

    Add get_user_by_username function

--- user.php
+++ user.php
@@ -11,6 +11,15 @@ Class User {
     $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
   }
+
+  public function get_user_by_username($username) {
+    $dbh = db_connect();
+    $statement = $dbh->prepare("select * from users where username = :username;");
+    $statement->bindParam(':username', $username);
+    $statement->execute();
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    return $row;
+  }
   
 }
 
