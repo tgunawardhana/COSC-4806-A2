71c6ab3797b6a28d4d021efb6dcfb8460e19b6e6
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-29 01:48:33 +0000

    Copied code from Assignment 1

--- index.php
+++ index.php
@@ -2,10 +2,42 @@
 
 require_once ('./user.php');
 
+/*
+
 $user = new User();
 $user_list = $user->get_all_users();
 
 echo "<pre>";
 print_r($user_list);
 
+*/
+
+
+
+session_start();
+  if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] != 1){
+    header("location: /login.php");
+  }
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>COSC 4806</title>
+  </head>
+  <body>
+
+    <h1>Assignment 1</h1>
+
+    <p>Welcome, <?= $_SESSION['username'] ?>!</p>
+    <p> <?= date("Y-M-d") ?> </p>
+
+    <!--
+    <p> <a href="/login.php">Click here to login</a> </p>
+    -->
+  </body>
+
+  <footer>
+    <p> <a href="/logout.php">Logout</a> </p>
+  </footer>
+</html>
