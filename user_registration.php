1cc85fb997e581d5e07c7ab8750c64c9353d9657
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-29 18:39:19 +0000

    Password validation code
    Added code to validate password length and match the two password fields to confirm the password

--- user_registration.php
+++ user_registration.php
@@ -5,12 +5,25 @@ session_start();
 
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
+$password2 = $_REQUEST['password2'];
 
 if ($username == "" || $password == "") {
     $_SESSION['error_signup'] = 1;
     header("location: /signup.php");
     return;
 }
+   
+else if (strlen($password) < 10){
+    $_SESSION['error_signup'] = 2;
+    header("location: /signup.php");
+    return;
+}
+else if ($password != $password2){
+    $_SESSION['error_signup'] = 3;
+    header("location: /signup.php");
+    return;
+}
+
 $_SESSION['failed'] = 0;
 $user = new User();
 $user->register_user($username, $password);
