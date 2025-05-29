5e3c2906b72b2783b0448bba208c774cb015a3ea
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-29 04:14:58 +0000

    Updated code to validate empty data

--- validate.php
+++ validate.php
@@ -17,7 +17,6 @@ $user_data = $user->get_user_by_username($_REQUEST['username']);
 $valid_username = $user_data['username'];
 $valid_password = $user_data['password'];
 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 if ($valid_username == $username && password_verify($password, $valid_password)){
     $_SESSION['authenticated'] = 1;
