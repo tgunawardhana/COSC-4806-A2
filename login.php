7244c94b816e6df0ed2739161ec03c926404154c
tgunawardhana <tgunawardhana@algomau.ca>
2025-05-29 04:15:30 +0000

    Updated code to show error if username or password is empty

--- login.php
+++ login.php
@@ -4,9 +4,11 @@
   if (isset($_SESSION['failed']) && $_SESSION['failed'] == 1) {
     $failed = "Username or password is incorrect.
     Unsuccessful attempts " . $_SESSION['failed_attempts'] . ".";
+    
   }
   else if (isset($_SESSION['error_data']) && $_SESSION['error_data'] == 1) {
     $failed = "Username or password cannot be empty.";
+    $_SESSION['error_data'] = 0;
   }
   else {
     $failed = "";
